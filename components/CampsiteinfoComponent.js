4840e5a1701111fd79e31cfaaade940af73f1d73
andre <78390294+ankrudov@users.noreply.github.com>
2021-08-06 14:18:02 -0500

    added animatable components to all directory components

--- components/CampsiteinfoComponent.js
+++ components/CampsiteinfoComponent.js
@@ -5,6 +5,7 @@ import {postComment} from '../redux/ActionCreators';
 import { postFavorite } from '../redux/ActionCreators';
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
+import * as Animatable from 'react-native-animatable';
 
 
 const mapStateToProps = state =>{
@@ -24,32 +25,34 @@ function RenderCampsite(props){
     const {campsite} = props;
     if(campsite){
         return (
-            <Card 
-                featuredTitle={campsite.name}
-                image={{uri:baseUrl + campsite.image}}>
-                <Text style={{margin:10}}>
-                    {campsite.description}
-                </Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={props.favorite ? 'star' : 'star-o'}
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card 
+                    featuredTitle={campsite.name}
+                    image={{uri:baseUrl + campsite.image}}>
+                    <Text style={{margin:10}}>
+                        {campsite.description}
+                    </Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={props.favorite ? 'star' : 'star-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            raised
+                            reverse
+                            onPress={()=>props.favorite ? 
+                            console.log('Already a favorite') : props.markFavorite()}
+                        />
+                        <Icon
+                        name='pencil'
                         type='font-awesome'
-                        color='#f50'
-                        raised
+                        color='#5637DD'
+                        raised 
                         reverse
-                        onPress={()=>props.favorite ? 
-                        console.log('Already a favorite') : props.markFavorite()}
-                    />
-                    <Icon
-                    name='pencil'
-                    type='font-awesome'
-                    color='#5637DD'
-                    raised 
-                    reverse
-                    onPress={()=>{props.onShowModal()}}
-                    />
-                </View>
-            </Card>
+                        onPress={()=>{props.onShowModal()}}
+                        />
+                    </View>
+                </Card>
+            </Animatable.View>
         );
     }
     return <View />;
@@ -73,12 +76,14 @@ function RenderComments({comments}){
     };
 
     return(
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                keyExtractor={item=>item.id.toString()}
-                renderItem={renderCommentItem}/>
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    keyExtractor={item=>item.id.toString()}
+                    renderItem={renderCommentItem}/>
+            </Card>
+        </Animatable.View>
     );
 }
 class CampsiteInfo extends Component{
